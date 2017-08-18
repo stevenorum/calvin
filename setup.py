75b614bc6c9369742f7ff69c047a2576c6e80c1e
Steve Norum <stevenorum@gmail.com>
2017-08-18 09:47:19 -0400

    Need this on another computer.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3                                                                                                                                   
+
+from setuptools import setup
+
+setup(name='calvin',
+      version='0.1.0',
+      description='Random python utilities.',
+      author='Steve Norum',
+      author_email='stevenorum@gmail.com',
+      url='www.stevenorum.com',
+      packages=['calvin'],
+      package_dir={'calvin': 'calvin'},
+     )
