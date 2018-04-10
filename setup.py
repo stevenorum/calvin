dffc9c3e48545e60af9ca58c6b57d6d497c8dc70
Steve Norum <stevenorum@gmail.com>
2018-04-10 15:36:06 -0400

    Adding a couple things and bumping version.

--- setup.py
+++ setup.py
@@ -2,15 +2,21 @@
 
 from distutils.core import setup
 
+MAJOR_VERSION='0'
+MINOR_VERSION='1'
+PATCH_VERSION='2'
+
+VERSION = "{}.{}.{}".format(MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION)
+
 setup(
     name = 'calvin',
     packages = ['calvin','calvin/aws'],
-    version = '0.0.1',
+    version = VERSION,
     description = 'Random python utilities.',
     author = 'Steve Norum',
     author_email = 'sn@littlestviking.com',
     url = 'https://github.com/stevenorum/calvin',
-    download_url = 'https://github.com/stevenorum/calvin/archive/0.1.tar.gz',
+    download_url = 'https://github.com/stevenorum/calvin/archive/{}.tar.gz'.format(VERSION),
     keywords = ['utils','aws','lambda','cli'],
     classifiers = [],
 )
